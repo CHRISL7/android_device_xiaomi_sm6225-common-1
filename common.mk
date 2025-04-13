1264cb425c5a4b28e2fec64f4509ed059b14cb5a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-04-13 13:39:59 +0530

    sm6225-common: Disable some log spam
    Change-Id: If9478b14260e79b27cd27735e17a1086232f69be
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- common.mk
+++ common.mk
@@ -293,6 +293,24 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.lmk.upgrade_pressure=40 \
     ro.lmk.use_minfree_levels=true
 
+# Logging
+SPAMMY_LOG_TAGS := \
+    SDM \
+    SDM-histogram \
+    SRE \
+    WifiHAL \
+    cnss-daemon \
+    sensors \
+    sensors-hal \
+    vendor.qti.bluetooth@1.0-ibs_handler \
+    vendor.qti.bluetooth@1.0-wake_lock \
+    vendor.qti.hardware.display.composer-service
+
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_VENDOR_PROPERTIES += \
+    $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=E)
+endif
+
 # Media
 MSM_VIDC_TARGET_LIST := bengal
 
